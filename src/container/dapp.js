39a809769ad55526db7ef6c41961de40126915b8
Hrithik28 <hrithik.a.sharma.28@gmail.com>
2020-06-14 00:11:30 +0530

    First Storage. No modifications

---
+++ src/container/dapp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from '../component/layout.js';
+import Login from '../component/login.html';
+import '../App.css';
+class Dapp extends Component {
+    render() {
+        return (
+            <Layout />
+        );
+    }
+}
+export default Dapp;
---
+++ src/middleware/credentials.js
@@ -0,0 +1,8 @@
+//You need to update each credentials as displayed on your moibit api dashboard.
+
+export default {
+    "API_KEY": "12D3KooWG1EgwrAEacUYrEonhCBmxL39fSz8KEmDoRS6JQ53RUKa", //YOUR API KEY
+    "API_SECRET": "080112402483b361ac71a7676236b8670b518e48066888974d305b66345a6399fca0ede85bef0db9a68533d0822626c9593f6a7d0c2bdbf7bba8d5c12a87a43de9fceca1", //YOUR API SECRET
+    "ADMIN": "", // ANY ACCOUNT OF YOUR WISH
+    "CUSTOM_URL" : "https://kfs2.moibit.io" //URL YOU HAVE BEEN ASSIGNED TO
+}
